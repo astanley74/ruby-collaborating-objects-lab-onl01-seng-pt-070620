c1b67a72f9158de0641b52f540d189df10952745
Austin Stanley <stanley.377@buckeyemail.osu.edu>
2020-07-28 21:16:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,8 +11,13 @@ class Song
     @@all
   end
   
-  def self.new_by_filename(filename)
-    artist_name = filename.split(" - ")[0]
+  def self.new_by_filename(filename) 
+    artist_name = filename.split(" - ")[0] 
+    song_name = filename.split(" - ")[1] 
+    song = Song.new(song_name) 
+    artist = Artist.find_or_create_by_name(artist_name) 
+    artist.songs << song 
+    song 
   end
   
   def artist_name=(name)
